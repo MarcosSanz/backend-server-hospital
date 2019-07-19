6b6dd3ed3dbd697a2319aca094eb815be0ab4c86
MarcosS <sanzgonzalezmarcos@gmail.com>
2019-07-19 17:18:44 +0200

    Fin de la sección 11

--- app.js
+++ app.js
@@ -19,13 +19,12 @@ var hospitalRoutes = require('./routes/hospital');
 var medicoRoutes = require('./routes/medico');
 var busquedaRoutes = require('./routes/busqueda');
 var uploadRoutes = require('./routes/upload');
+var imagenesRoutes = require('./routes/imagenes');
 
 // Conexión a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
-
   if (err) throw err;
   console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
-
 });
 
 // Rutas
@@ -35,6 +34,7 @@ app.use('/medico', medicoRoutes);
 app.use('/busqueda', busquedaRoutes);
 app.use('/login', loginRoutes);
 app.use('/upload', uploadRoutes);
+app.use('/img', imagenesRoutes);
 // Debe ser la última ruta.
 app.use('/', appRoutes);
 
